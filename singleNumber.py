17b53fdd6f2f5c32719bb84203129f4061be22b1
Saiteja1620 <nomulasaiteja733@gmail.com>
2026-01-31 23:36:56 +0530

    Leetcode: number with one occurence with bit manipulation method

--- singleNumber.py
+++ singleNumber.py
@@ -1,12 +1,20 @@
 #find the element that appears only once in an array where every other element appears twice
+#method 1: using dictionary to count occurrences
+# array=list(map(int,input().split()))
+# d={}
+# for i in array:
+#     if i not in d:
+#         d[i]=1
+#     else:
+#         d[i]+=1
+# print(d)
+# for i in d:
+#     if d[i]==1:
+#         print(i)
+
+#method 2: using XOR operation
 array=list(map(int,input().split()))
-d={}
+result=0
 for i in array:
-    if i not in d:
-        d[i]=1
-    else:
-        d[i]+=1
-print(d)
-for i in d:
-    if d[i]==1:
-        print(i)
+    result^=i
+print(result)
